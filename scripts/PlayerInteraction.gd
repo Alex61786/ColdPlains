c9d3935c7eb017031ead133f9ba543a769cb9daa
Qianwen-Tan <0158799@schoolsnet.act.edu.au>
2026-07-24 11:40:41 +1000

    Add PlayerInteraction.gd for handling E key pickups

---
+++ scripts/PlayerInteraction.gd
@@ -0,0 +1,65 @@
+extends Area3D
+class_name PlayerInteraction
+
+@export var pickup_range: float = 3.0
+
+var player_id: int
+var nearby_items: Array[Item] = []
+var inventory: PlayerInventory
+
+func _ready() -> void:
+	# Get the parent player node
+	var player = get_parent()
+	player_id = player.name.to_int()
+	
+	# Get or create inventory
+	inventory = player.get_node_or_null("PlayerInventory")
+	if not inventory:
+		print("Warning: PlayerInventory not found on player!")
+	
+	# Set up area signals
+	area_entered.connect(_on_area_entered)
+	area_exited.connect(_on_area_exited)
+	
+	if not is_multiplayer_authority():
+		return
+
+func _physics_process(_delta: float) -> void:
+	if not is_multiplayer_authority():
+		return
+	
+	if Input.is_action_just_pressed("interact"):
+		attempt_pickup()
+
+func attempt_pickup() -> void:
+	# Find closest item within range
+	var closest_item: Item = null
+	var closest_distance: float = pickup_range
+	
+	for item in nearby_items:
+		if not item.is_picked_up:
+			var distance = global_position.distance_to(item.global_position)
+			if distance < closest_distance:
+				closest_distance = distance
+				closest_item = item
+	
+	if closest_item:
+		if closest_item.pick_up(player_id):
+			# Add item to inventory
+			if inventory:
+				inventory.add_item(closest_item)
+			print("Picked up: ", closest_item.item_name)
+
+func _on_area_entered(area: Area3D) -> void:
+	if area is Item:
+		if area not in nearby_items:
+			nearby_items.append(area)
+			print("Item nearby: ", area.item_name)
+
+func _on_area_exited(area: Area3D) -> void:
+	if area is Item:
+		nearby_items.erase(area)
+		print("Item out of range: ", area.item_name)
+
+func get_nearby_items() -> Array[Item]:
+	return nearby_items
