b2bf5dadc961291398c0368f18353bd6a37e54dc
Qianwen-Tan <0158799@schoolsnet.act.edu.au>
2026-08-19 14:28:02 +1000

    item pick and inventory functioned

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -53,6 +53,7 @@ func _enter_tree():
 	set_multiplayer_authority(str(name).to_int())
 
 func _ready():
+	rpc("flashLight_toggle")
 	if not is_multiplayer_authority(): 
 		return
 	
@@ -105,13 +106,11 @@ func _unhandled_input(event):
 	elif Input.is_action_just_released("shoot"):
 		is_shooting = false
 
-
-	
 func _physics_process(delta):
 	if not is_multiplayer_authority() or is_dead: 
 		return
 	
-	_handle_crouch(delta)
+	handle_crouch(delta)
 	
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -234,8 +233,6 @@ func play_shoot_effects():
 		else:
 			print("Uzi muzzle flash is null")
 
-
-
 @rpc("any_peer", "call_local")
 func receive_damage():
 	print("receive_damage() called on player:", name)
@@ -251,7 +248,7 @@ func receive_damage():
 		is_dead = true
 		rpc("die")
 
-func _handle_crouch(delta) -> void:
+func handle_crouch(delta) -> void:
 	if is_crouched: 
 		SPEED = crouch_speed 
 		JUMP_VELOCITY = crouch_jump_velocity
@@ -274,3 +271,10 @@ func die():
 	hide()
 	if is_multiplayer_authority():
 		get_tree().call_group("ui","show_lose_screen")
+
+@rpc("authority", "call_local")
+func flashLight_toggle():
+	if Input.is_action_just_pressed("toggle_flashlight"):
+		flashlight.visible = not flashlight.visible
+	if is_multiplayer_authority():
+		flashlight.visible = not flashlight.visible
